fd3bbd4d6acabbab0070adef5dc3b4e25f993988
Pavel Kozlov <kozlov.pavel.s@gmail.com>
2020-07-13 20:08:12 +0200

    add feedbackService

---
+++ src/services/feedbackService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { API_ENDPOINT } from '../constants/appConstants.js';
+
+const instance = axios.create({
+  baseURL: API_ENDPOINT,
+});
+
+/**
+ * Fetches the feedback items.
+ *
+ * @returns {Promise<T>}
+ */
+const fetchItems = () =>
+  instance.get('example/apidemo.json')
+    .then((response) => response.data);
+
+export {
+  fetchItems,
+}
