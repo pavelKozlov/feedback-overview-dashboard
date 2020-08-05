5d0d74ec5a7467c69ee7a44f0acf13eaa9fa9568
Pavel Kozlov <kozlov.pavel.s@gmail.com>
2020-08-05 21:06:55 +0200

    change list max height

--- src/components/feedbackList/FeedbackList.jsx
+++ src/components/feedbackList/FeedbackList.jsx
@@ -6,19 +6,21 @@ import { FeedbackListHeader } from './feedbackListHeader/FeedbackListHeader.jsx'
 const FeedbackList = ({ items }) => (
   <div className="feedback-list">
     <FeedbackListHeader />
-    <ul className="feedback-list__list">
-      {items.map((item) => (
-        <FeedbackListItem
-          {...{
-            key: item.id,
-            rating: item.rating,
-            comment: item.comment,
-            userAgent: item.browser.userAgent,
-            computedBrowser: item.computed_browser,
-          }}
-        />
-      ))}
-    </ul>
+    <div className="feedback-list__list-container">
+      <ul className="feedback-list__list">
+        {items.map((item) => (
+          <FeedbackListItem
+            {...{
+              key: item.id,
+              rating: item.rating,
+              comment: item.comment,
+              userAgent: item.browser.userAgent,
+              computedBrowser: item.computed_browser,
+            }}
+          />
+        ))}
+      </ul>
+    </div>
   </div>
 );
 
