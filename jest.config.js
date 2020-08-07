d1af8d71e1802f43695546037d41bbc06cb5d7fe
Pavel Kozlov <kozlov.pavel.s@gmail.com>
2020-08-07 21:23:42 +0200

    add tests for Filter input component

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'roots': [
+    '<rootDir>/src'
+  ],
+  /* 'transform': {
+    '^.+\\.jsx?$': 'ts-jest'
+  }, */
+  'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+  'moduleFileExtensions': [
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  'snapshotSerializers': ['enzyme-to-json/serializer'],
+};
---
+++ src/components/filterInput/__tests__/FilterInput.spec.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import {stub} from 'sinon';
+
+import {FilterInput} from '../FilterInput.jsx';
+
+describe('<FilterInput/>', () => {
+  let onFilterItems;
+  let onClearClicked;
+
+  beforeEach(() => {
+    onFilterItems = stub();
+    onClearClicked = stub();
+  });
+
+  const mountComponent = (props = {}) => shallow(
+    <FilterInput {...{
+      placeholder: 'Test placeholder',
+      filterStr: 'test filter string',
+      onFilterItems,
+      onClearClicked,
+      ...props
+    }}
+    />
+  );
+
+  it('should render <FilterInput/> component', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.hasClass('filter-input')).toBe(true);
+  });
+
+  it('should render input element inside <FilterInput/> component', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.find('input').length).toBe(1);
+    const input = wrapper.find('input');
+    expect(input.hasClass('filter-input__input')).toBe(true);
+    expect(input.prop('placeholder')).toBe('Test placeholder');
+    expect(input.prop('value')).toBe('test filter string');
+  });
+
+  it('should render clear button element inside <FilterInput/> component', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.find('button').length).toBe(1);
+    const button = wrapper.find('button').first();
+    expect(button.hasClass('filter-input__clear-button')).toBe(true);
+    expect(button.text()).toBe('Clear');
+  });
+
+  it('should call onFilterItems method on input element value changes', () => {
+    const input = mountComponent().find('input');
+    input.simulate('change', {target: {value: 'abc'}});
+
+    expect(onFilterItems.calledOnce).toBe(true);
+    expect(onFilterItems.getCall(0).args[0]).toEqual({target: {value: 'abc'}});
+  });
+
+  it('should call onClearClicked method on clear button click', () => {
+    const button = mountComponent().find('button');
+    button.simulate('click');
+
+    expect(onClearClicked.calledOnce).toBe(true);
+  });
+});
